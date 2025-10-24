e8c44f682d664781f4bc52a99591f6b781bffddb
Komodovic <103980435+Komodovic@users.noreply.github.com>
2025-10-24 11:21:39 +0700

    Add files via upload

---
+++ Perulangan.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	int jumGenap = 0, jumGanjil = 0;
+	
+	cout << "Masukkan jumlah data: ";
+	cin >> n;
+
+	for (int i = 1; i < n; i++) {
+		int data;
+		cout << "Masukkan data ke-" << i << ": ";
+		cin >> data;
+
+		if (data % 2 == 0) {
+			jumGenap += data;
+		}
+		else {
+			jumGanjil += data;
+		}
+	}
+
+	cout << "Jumlah bilangan genap = " << jumGenap << "\n";
+	cout << "Jumlah bilangan ganjil = " << jumGanjil << "\n";
+
+
+	return 0;
+}
